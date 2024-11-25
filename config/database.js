451734f038efcc4b51b36224a933473b441b7d8b
Cole W. <coleworldsports@gmail.com>
2024-11-24 22:19:57 -0500

    updated package.json

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://julian:PqiSPIoTcn3kEg1m@rc-projects.wm56a.mongodb.net/?retryWrites=true&w=majority&appName=RC-Projects', 
+    'url' : "mongodb+srv://julian:BTKELuJ1WzgROo3f@rc-projects.wm56a.mongodb.net/?retryWrites=true&w=majority&appName=RC-Projects", 
     'dbName': 'personal-express'
 };
 
